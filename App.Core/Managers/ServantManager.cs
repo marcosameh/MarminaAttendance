aaf89e33f29880bd6c46f30097bac46c1af3056c
Marco <marco@innovixsolutons.com>
2023-03-18 23:20:30 +0200

    setup Servant List page

--- App.Core/Managers/ServantManager.cs
+++ App.Core/Managers/ServantManager.cs
@@ -1,4 +1,5 @@
 ﻿using App.Core.Entities;
+using App.Core.Models;
 using Microsoft.EntityFrameworkCore;
 using SharedKernel.Core.Common;
 
@@ -7,7 +8,7 @@ namespace App.Core.Managers
     public class ServantManager
     {
         private readonly MarminaAttendanceContext _context;
-        private int NumberOfWeeksAppearInMarkup=5;
+        private int NumberOfWeeksAppearInMarkup = 5;
 
         public ServantManager(MarminaAttendanceContext context)
         {
@@ -27,59 +28,21 @@ namespace App.Core.Managers
             }
 
         }
-        public Result UpdateClass(Classes ClassData, Dictionary<int, List<int>> checkedServantWeeks)
+
+        public List<ServantVM> GetServants()
         {
-            // Retrieve the existing class and update its properties
-            var existingClass = _context.Classes
-                .Include(c => c.Servants).ThenInclude(x => x.ServantWeek)
-                .SingleOrDefault(c => c.Id == ClassData.Id);
-
-            if (existingClass == null)
+            var Servants = _context.Servants.Include(s => s.Class).AsNoTracking().Select(x=> new ServantVM
             {
-                // Return an error if the class does not exist
-                return Result.Fail("Class not found");
-            }
-
-            existingClass.Name = ClassData.Name;
-            existingClass.Intercessor = ClassData.Intercessor;
-            existingClass.TimeId = ClassData.TimeId;
-
-            // Update the ServantWeek records for each servant
-            var allWeeks = _context.Weeks.OrderByDescending(x => x.Id).Take(NumberOfWeeksAppearInMarkup);
-            foreach (var servant in existingClass.Servants)
-            {
-                // Get the list of checked week ids for the current servant
-                var checkedWeekIds = checkedServantWeeks.GetValueOrDefault(servant.Id, new List<int>());
-                // Loop through all weeks, including those not displayed on the form
-                foreach (var week in allWeeks)
-                {
-                    // Check if the checkbox for this week was checked
-                    var weekChecked = checkedWeekIds.Contains(week.Id);
-
-                    // Check if the current servant already has a ServantWeek object for this week
-                    var existingServantWeek = servant.ServantWeek.FirstOrDefault(x => x.WeekId == week.Id);
-
-                    if (weekChecked && existingServantWeek == null)
-                    {
-                        // Checkbox was checked and there is no existing ServantWeek object, so create a new one
-                        var newServantWeek = new ServantWeek { ServantId = servant.Id, WeekId = week.Id };
-                        _context.ServantWeek.Add(newServantWeek);
-                    }
-                    else if (!weekChecked && existingServantWeek != null)
-                    {
-                        // Checkbox was unchecked and there is an existing ServantWeek object, so delete it
-                        _context.ServantWeek.Remove(existingServantWeek);
-                    }
-                }
-
-            }
-
-
-            // Save changes to the database
-            _context.SaveChanges();
-            return Result.Ok("Class updated successfully");
-
-
+                Id=x.Id,
+                Name=x.Name,
+                Address=x.Address,
+                Birthday=x.Birthday,
+                ClassName=x.Class.Name,
+                FatherOfConfession=x.FatherOfConfession,
+                Phone=x.Phone,
+                Photo=x.Photo,
+            }).ToList();
+            return Servants;
         }
 
 
--- AppAdmin/Pages/Servants/List.cshtml.cs
+++ AppAdmin/Pages/Servants/List.cshtml.cs
@@ -1,3 +1,6 @@
+using App.Core.Entities;
+using App.Core.Managers;
+using App.Core.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,32 @@ namespace App.UI.Pages.Servant
 {
     public class ListModel : PageModel
     {
+        private readonly ServantManager servantManager;
+        private readonly ClassManager classManager;
+
+        public List<ServantVM> ServantsVM { get; private set; }
+        public List<ClassVM> Classes { get; private set; }
+
+        [BindProperty] 
+        public Servants Servant { get; set; }
+        public ListModel(ServantManager servantManager,ClassManager classManager)
+        {
+            this.servantManager = servantManager;
+            this.classManager = classManager;
+        }
         public void OnGet()
         {
+            FillData();
+        }
+        public IActionResult OnGetDisplayServants()
+        {
+            ServantsVM = servantManager.GetServants();
+
+            return new JsonResult(ServantsVM);
+        }
+        public void FillData()
+        {
+            Classes=classManager.GetClasses();
         }
     }
 }
