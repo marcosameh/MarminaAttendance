f5650c9d30b62cab6d4ca2167b15ce4bdc2db2f6
Marco.Sameh <Marco.Sameh@MARCO-SAMEH>
2025-04-04 14:02:07 +0200

    updates

--- App.UI/Pages/Servants/card.cshtml.cs
+++ App.UI/Pages/Servants/card.cshtml.cs
@@ -1,3 +1,4 @@
+using App.Core.Entities;
 using App.Core.Managers;
 using App.Core.Models;
 using MarminaAttendance.Identity;
@@ -6,31 +7,30 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace App.UI.Pages.Classes
+namespace App.UI.Pages.Servant
 {
     [Authorize]
     public class printModel : PageModel
     {
-        private readonly ServedManager _servedManager;
         private readonly ServantManager _servantManager;
 
-        [BindProperty(SupportsGet = true)] public int? classId { get; set; }
+        [BindProperty(SupportsGet = true)] public int? servantId { get; set; }
 
-        public List<ServedVM> Serveds { get; set; }
-        public List<ServantVM> Servants { get; set; }
-        public printModel(ServedManager servedManager,
+        public Servants Servant { get; set; }
+     
+        public printModel(
             ServantManager servantManager)
 
         {
-            _servedManager = servedManager;
+           
             _servantManager = servantManager;
         }
         public void OnGet()
         {
-            if (classId.HasValue && classId.Value > 0)
+            if (servantId.HasValue && servantId.Value > 0)
             {               
-                Servants = _servantManager.GetServants(classId.Value);
-                Serveds = _servedManager.GetServeds(classId.Value);
+                Servant = _servantManager.GetServant(servantId.Value);
+               
             }
         }
     }
--- App.UI/Pages/Served/card.cshtml.cs
+++ App.UI/Pages/Served/card.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace App.UI.Pages.Classes
+namespace App.UI.Pages.Serveds
 {
     [Authorize]
     public class printModel : PageModel
